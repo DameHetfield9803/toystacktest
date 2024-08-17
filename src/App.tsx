6ace955d14f2e1273a6b74aa7c84d50878efe82a
DameHetfield9803 <damiendkfoo@yahoo.com.sg>
2024-08-17 12:14:55 +0800

    oeqwkeoqwkeowqkeoqkeopwqiewqoejwqoenqwkejwqkejwqioejwqoejwqoeowqjueowq

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+export default function App() {
+  return (
+    <div className="bg-black mx-auto container flex flex-col text-center text-white min-w-full min-h-screen">
+      <div className='p-3'>
+        <h1>Hello world</h1>
+        <p>This is a test for Toystack.ai</p>
+      </div>
+    </div>
+  )
+}
